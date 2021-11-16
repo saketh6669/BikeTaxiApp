8f7ef4780c2063c2ffe4009d92c7ea1d3e175fc0
ksaketh <saketh6669@gmail.com>
2021-11-16 17:26:26 +0530

    first commit

---
+++ BikeTaxiApp/CustomRenderer/MyCustomerEntry.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BikeTaxiApp
+{
+    public partial class MyCustomerEntry : ContentPage
+    {
+        public MyCustomerEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
